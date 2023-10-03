2762a6cc865f45235af739c7ca655c1a16af5e10
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-10-03 13:35:24 +0530

    Node introduction

---
+++ globals.js
@@ -0,0 +1,14 @@
+//Globals -No Window;
+//__dirname -Path to current directory;
+//__filename -return file name;
+//require -function to use modules (CommonJS);
+//module -info about current modules;
+//process -info about env where the program is being executed;
+
+
+console.log(__dirname);
+console.log(__filename);
+
+setTimeout(() => {
+    console.log("hnn bhyii kesi rahi");
+}, 5000);
---
+++ introNodeConcepts.js
@@ -0,0 +1,10 @@
+let a = 99;
+
+if(a<10){
+    console.log("Small Number");
+}
+else{
+    console.log("Large Number");
+}
+console.log("this is my first node app!!!");
+
